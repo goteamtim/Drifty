9f37bedfc0ca1c11f42a0a6bcec33e933923eece
Tim Harshman <goteamtim+git@gmail.com>
2017-03-22 19:55:50 -0700

    Setup some server stuffs

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var Flickr = require('flickrapi'),
-flickrOptions = {
-      api_key: "",
+    flickrOptions = {
+        api_key: "",
       secret: ""
     };
- 
-Flickr.authenticate(flickrOptions, function(error, flickr) {
-  // we can now use "flickr" as our API object
-});
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+Flickr.authenticate(flickrOptions, function (error, flickr) {
+    // we can now use "flickr" as our API object
+    //Handle error here
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+    app.get('/driftly', function (req, res) {
+        flickr.photos.search({
+            text: "red+panda"
+        }, function (err, result) {
+            if (err) { throw new Error(err); }
+            // do something with result
+            res.send(result);
+        });
+
+    });
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!');
+    });
+
+
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
+
+
 
